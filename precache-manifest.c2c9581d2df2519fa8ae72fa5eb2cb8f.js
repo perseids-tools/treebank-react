e01ff0ff93341e6dce8a8c7955b29958275042f2
zfletch <zfletch2@gmail.com>
2020-10-02 17:42:43 -0400

    Updates

---
+++ precache-manifest.c2c9581d2df2519fa8ae72fa5eb2cb8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa031d114ab658d8451e7c7ebc7b4582",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18154bd71639b1708ac9",
+    "url": "/static/css/main.b928946e.chunk.css"
+  },
+  {
+    "revision": "5d4013881e9936e0bdd0",
+    "url": "/static/js/2.b161e444.chunk.js"
+  },
+  {
+    "revision": "4821c8a20c0bb87dc17fbd2c2889c2e0",
+    "url": "/static/js/2.b161e444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18154bd71639b1708ac9",
+    "url": "/static/js/main.37cdefe7.chunk.js"
+  },
+  {
+    "revision": "ad0f525f25a028ce55d5",
+    "url": "/static/js/runtime-main.e278bac4.js"
+  }
+]);
