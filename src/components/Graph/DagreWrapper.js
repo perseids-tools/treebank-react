e7861b2d20087710030a8ffac677a2c6d12a340f
zfletch <zfletch2@gmail.com>
2020-10-29 19:55:30 -0400

    fix resizing issue

--- src/components/Graph/DagreWrapper.js
+++ src/components/Graph/DagreWrapper.js
@@ -9,7 +9,9 @@ const config = {
   rankdir: 'TB',
 };
 
-const drawGraph = (svg, g, { nodes, links, onClick }) => {
+const drawGraph = (svg, g, {
+  nodes, links, onClick, transform,
+}) => {
   const graph = new dagreD3.graphlib.Graph().setGraph(config);
 
   nodes.forEach(({
@@ -30,13 +32,19 @@ const drawGraph = (svg, g, { nodes, links, onClick }) => {
   const renderer = new dagreD3.render();
   const selectedSvg = d3.select(svg.current);
   const selectedG = d3.select(g.current);
-  const zoom = d3.zoom().on('zoom', (event) => selectedG.attr('transform', event.transform));
+  const zoom = d3.zoom().on('zoom', (event) => {
+    // eslint-disable-next-line no-param-reassign
+    transform.current = event.transform;
+    selectedG.attr('transform', event.transform);
+  });
 
   selectedSvg.call(zoom);
 
+  selectedG.attr('transform', d3.zoomIdentity);
   renderer(selectedG, graph);
+  selectedG.attr('transform', transform.current);
 
-  selectedSvg.selectAll('g.node').on('click', ({ target }) => {
+  selectedSvg.selectAll('g.node').on('mousedown', ({ target }) => {
     // eslint-disable-next-line no-underscore-dangle
     onClick(target.__data__);
   });
@@ -46,7 +54,17 @@ const DagreWrapper = ({ nodes, links, onClick }) => {
   const svg = useRef(null);
   const g = useRef(null);
 
-  useEffect(() => drawGraph(svg, g, { nodes, links, onClick }));
+  // See https://github.com/dagrejs/dagre-d3/issues/251
+  // There is a bug in Dagre D3 (0.6.4) which causes positioning issues
+  // when a user zooms and then the graph is re-rendered. This variable
+  // keeps track of the zoom level and positioning. When a re-render occurs,
+  // the code first sets the zoom and and position to the default, then
+  // re-renders, and finally resets the zoom to what it was before.
+  const transform = useRef(d3.zoomIdentity);
+
+  useEffect(() => drawGraph(svg, g, {
+    nodes, links, onClick, transform,
+  }));
 
   return (
     <svg ref={svg} className={styles.graph}>
