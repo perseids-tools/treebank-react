6494f9c362fadf29181a29f32f1f2ec09a20e14c
zfletch <zfletch@users.noreply.github.com>
2020-10-31 00:31:45 +0000

    Deploying to gh-pages from  @ 2f7f1a82ff38f061c190d74ebfba249232097056 🚀

---
+++ precache-manifest.dde3887f0e8e05b234ba5bd57a27c7fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "013069f16d0f8fe835497efcafca0f61",
+    "url": "/treebank-react/index.html"
+  },
+  {
+    "revision": "172143c9852fd94ef366",
+    "url": "/treebank-react/static/css/main.58aa6a3c.chunk.css"
+  },
+  {
+    "revision": "134eda3ddcd8cccbe466",
+    "url": "/treebank-react/static/js/2.625a1eee.chunk.js"
+  },
+  {
+    "revision": "172143c9852fd94ef366",
+    "url": "/treebank-react/static/js/main.41f94e62.chunk.js"
+  },
+  {
+    "revision": "0b3e9356131c58e9de28",
+    "url": "/treebank-react/static/js/runtime-main.cd916f0e.js"
+  }
+]);
